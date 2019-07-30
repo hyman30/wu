b38ae28bb992c08742fa7b2bef5e558fef45ced7
hyman30 <85606298@qq.com>
2019-07-30 15:01:49 +0800

    code

---
+++ jump7.py
@@ -0,0 +1,10 @@
+for i in range(0,100):
+    i+=1
+    if (i%7==0):
+        continue
+    if (i%10==7):
+        continue
+    if (i//10==7):
+        continue
+    print(i)
+                        
